8e871e608e3da9fb22ebef461efc2ef1cbbc7209
Leandro Lisboa Penz <lpenz@lpenz.org>
2018-05-19 16:28:47 +0100

    Follow standard license practices

--- test_watchng.py
+++ test_watchng.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2012 Leandro Lisboa Penz <lpenz@lpenz.org>
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
 '''Tests for watchng'''
 
 import unittest
